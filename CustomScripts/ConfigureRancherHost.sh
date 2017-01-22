390912cfbb786468e02b9ebadc1ec31b172a8b08
Adam Graves <adam.graves@nhs.net>
2017-01-22 00:50:19 +0000

    Fix scripts

--- CustomScripts/ConfigureRancherHost.sh
+++ CustomScripts/ConfigureRancherHost.sh
@@ -2,9 +2,13 @@
 export HOST_IP=$(ip addr show eth0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*')
 
 export RANCHER_SERVER_URL=$1
-export RANCHER_ENV=$2
-export RANCHER_TOKEN=$3
-export CATTLE_HOST_LABELS=$4
+export RANCHER_TOKEN=$2
 export RANCHER_AGENT_DOCKER_IMAGE='rancher/agent:v1.1.3'
 
-sudo docker run -e CATTLE_HOST_LABELS=${CATTLE_HOST_LABELS} -e CATTLE_AGENT_IP=${HOST_IP} -d --privileged -v /var/run/docker.sock:/var/run/docker.sock  ${RANCHER_AGENT_DOCKER_IMAGE} ${RANCHER_SERVER_URL}/v1/projects/${RANCHER_ENV}/scripts/${RANCHER_TOKEN}
+sudo docker run -e CATTLE_HOST_LABELS=${CATTLE_HOST_LABELS} -d --privileged -v /var/run/docker.sock:/var/run/docker.sock
+sudo docker run -d --privileged \
+                -e CATTLE_AGENT_IP=${HOST_IP} \
+                -v /var/run/docker.sock:/var/run/docker.sock \
+                -v /var/lib/rancher:/var/lib/rancher \
+                ${RANCHER_AGENT_DOCKER_IMAGE} \
+                ${RANCHER_SERVER_URL}/v1/scripts/${RANCHER_TOKEN}
