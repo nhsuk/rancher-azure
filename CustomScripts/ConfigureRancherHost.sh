4b69d0c02fd5d34c3be5329e9d20ef7c528e4cfd
Adam Graves <adam.graves@nhs.net>
2017-02-24 15:59:48 +0000

    Add host labels config

--- CustomScripts/ConfigureRancherHost.sh
+++ CustomScripts/ConfigureRancherHost.sh
@@ -3,6 +3,8 @@ export HOST_IP=$(ip addr show eth0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*'
 
 export RANCHER_SERVER_URL=$1
 export RANCHER_TOKEN=$2
+export RANCHER_LABELS=$3
+
 export RANCHER_AGENT_DOCKER_IMAGE='rancher/agent:v1.2.0'
 
 # INSTALL DOCKER
@@ -12,6 +14,7 @@ sleep 5
 # RUN RANCHER SERVER
 sudo docker run -d --privileged \
                 -e CATTLE_AGENT_IP=${HOST_IP} \
+                -e CATTLE_HOST_LABELS="'${RANCHER_LABELS}'" \
                 -v /var/run/docker.sock:/var/run/docker.sock \
                 -v /var/lib/rancher:/var/lib/rancher \
                 ${RANCHER_AGENT_DOCKER_IMAGE} \
